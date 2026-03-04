c2dd1d57bb6f95b4effbe9ddf5eb9a32e7c7da2b
angelomds42 <angelomds42@gmail.com>
2026-03-03 21:30:27 -0300

    bot: misc: Add id command
    Change-Id: I3c59b1b1f451f4a20b015a0acccd99d0c17f205d
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -5,6 +5,8 @@ from bot.utils.message import reply, edit
 
 import time
 
+from bot.utils.user import resolve_target
+
 
 async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
     s, e = get_string_helper(update)
@@ -21,7 +23,32 @@ async def dice(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.effective_chat.send_dice(emoji="🎲")
 
 
+async def id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    s, e = get_string_helper(update)
+
+    if not update.message.reply_to_message and not context.args:
+        return await reply(
+            update,
+            s(
+                "misc.id.self",
+                user_id=e(update.effective_user.id),
+                chat_id=e(update.effective_chat.id),
+            ),
+        )
+
+    user_id, display_name = await resolve_target(update, context)
+
+    if not user_id:
+        return await reply(update, s("misc.id.not_found"))
+
+    await reply(
+        update,
+        s("misc.id.user", display_name=e(display_name), user_id=e(user_id)),
+    )
+
+
 def __init_module__(application):
     application.add_handler(CommandHandler("ping", ping))
     application.add_handler(CommandHandler("dice", dice))
+    application.add_handler(CommandHandler("id", id))
     register_module_help("Misc", "misc.help")
